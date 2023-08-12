d8af4b19af284951d40a454d656028c7a78a158a
Marvin Berrio <marvinccp@gmail.com>
2023-08-12 12:08:38 +0200

    Portfolio Vanilla Js Version

---
+++ helpers/dataProjects.js
@@ -0,0 +1,101 @@
+export const dataProjects = [
+  {
+    id: "1",
+    title: "Master Question",
+    description:
+      "MQ es el prototipo de un juego sencillo de preguntas y respuestas tipo test",
+    link: "https://masterqgame.vercel.app/?vercelToolbarCode=q5J1LprEQR4zen1",
+    github: "https://github.com/marvinccp/masterqgame",
+    image: "../static/projects_images/master_question.png",
+    tec: [
+      { tec: "Javascript", image: "/icons/js.png" },
+      { tec: "CSS", image: "/icons/css.png" },
+      { tec: "Node Js", image: "/icons/node.png" },
+      { tec: "Next Js", image: "/icons/next.png" },
+    ],
+    tags: ["React", "Javascript", "CSS", "Game", "Node", "Web Developer"],
+    youtube:
+      "https://www.youtube.com/watch?v=sh-P1FFd7oA&list=PL0_LqhiRYs8Eau8DxJLy4xF1LzEoK3twE",
+  },
+
+  {
+    id: "2",
+    title: "YouTube Hub",
+    description:
+      "Este proyecto utiliza la API de Youtube para obtener información  mis listas  de reproducción favoritas.",
+    link: "https://youtube-hub.vercel.app/",
+    github: "https://github.com/marvinccp/youtubeHUB",
+    image: "../static/projects_images/youtube_hub.png",
+    tec: [
+      { tec: "Javascript", image: "/icons/js.png" },
+      { tec: "CSS", image: "/icons/css.png" },
+      { tec: "Next Js", image: "/icons/next.png" },
+    ],
+    tags: ["Next", "React", "Javascript", "CSS", "Web Developer"],
+    youtube: "https://www.youtube.com/@tevstudio",
+  },
+  {
+    id: "3",
+    title: "21 Black Jack",
+    description:
+      "Este juego es una adaptación del tipico black jack. he simplicado la interface",
+    link: "https://21game.vercel.app/",
+    github: "https://github.com/marvinccp/21game",
+    image: "../static/projects_images/21.png",
+    tec: [
+      { tec: "Javascript", image: "/icons/js.png" },
+      { tec: "CSS", image: "/icons/css.png" },
+      { tec: "React", image: "/icons/react.png" },
+    ],
+    tags: ["React", "Javascript", "CSS", "Web Developer"],
+    youtube: "https://www.youtube.com/@tevstudio",
+  },
+  {
+    id: "4",
+    title: "Fast Dogs",
+    description:
+      "Este proyecto es un demo básico de un sitio web de un restaurante de comidas rapidas.",
+    link: "https://fastdogs.vercel.app/",
+    github: "https://github.com/marvinccp/fastdogs",
+    image: "../static/projects_images/f_dogs.png",
+    tec: [
+      { tec: "Javascript", image: "/icons/js.png" },
+      { tec: "CSS", image: "/icons/css.png" },
+      { tec: "React", image: "/icons/react.png" },
+    ],
+    tags: ["React", "Javascript", "CSS", "Web Developer"],
+    youtube: "https://www.youtube.com/@tevstudio",
+  },
+  {
+    id: "5",
+    title: "SOUL",
+    description:
+      "Este prototipo se convertirá en una tienda online de ropa. este es uno de nuestro proyectos en familia a mediano plazo",
+    link: "https://soul.marvinberrio.com/",
+    github: "https://github.com/marvinccp/soul/tree/master/staticPage",
+    image: "../static/projects_images/soul.png",
+    tec: [
+      { tec: "Javascript", image: "/icons/js.png" },
+      { tec: "CSS", image: "/icons/css.png" },
+      { tec: "Html", image: "/icons/html.png" },
+    ],
+    tags: ["Javascript", "HTML5", "CSS", "Web Developer"],
+    youtube: "https://www.youtube.com/@tevstudio",
+  },
+  {
+    id: "6",
+    title: "Mercadoña",
+    description:
+      "Este proyecto es un demo de un programa de un supermercado para control de inventario",
+    link: "https://github.com/marvinccp/merca_prototipo",
+    github: "https://github.com/marvinccp/merca_prototipo",
+    image: "../static/projects_images/mercadora.png",
+    tec: [
+      { tec: "Postgres", image: "/icons/postgres.png" },
+      { tec: "Python", image: "/icons/python.png" },
+      { tec: "Django", image: "/icons/django.png" },
+    ],
+    tags: ["Django", "Python", "Postgres", "Web Developer"],
+    youtube: "https://www.youtube.com/@tevstudio",
+  },
+];
---
+++ scripts.js
@@ -0,0 +1,50 @@
+import { dataProjects } from "./helpers/dataProjects.js";
+
+// dataProjects.map( p => {
+
+//     const { title, description, image, link } = p;
+
+//     const div = document.createElement('div');
+//     div.classList.add('card');
+
+//     div.innerHTML = `
+//         <img src="${image}" alt="">
+//         <h3>${title}</h3>
+//         <p>${description}</p>
+//         <a href="${link}" target="_blank">Ver proyecto</a>
+//     `;
+
+//     document.querySelector('#projects').appendChild(div);
+// }
+
+// )
+
+dataProjects.map((p) => {
+  const { image, title } = p;
+
+  //projects container
+  const projectsContainer = document.querySelector(".projects-container");
+
+  //image container
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("image-container");
+
+  //image
+  const projectsImages = document.createElement("img");
+  projectsImages.src = image;
+
+  projectsImages.addEventListener("click", () => {
+    const modal = document.querySelector("#modal");
+    const modalText = document.querySelector("#text");
+    modalText.textContent = title;
+    modal.showModal();
+  });
+
+  imageContainer.appendChild(projectsImages);
+  projectsContainer.appendChild(imageContainer);
+});
+const closeModalButton = document.getElementById("closeModal");
+closeModalButton.addEventListener("click", () => {
+  const modal = document.getElementById("modal");
+  modal.close();
+});
